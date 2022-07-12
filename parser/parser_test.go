3fa7226ccc9c1204e3fa352ffc3378595f2f9db0
hayitsdavid <davehay93@gmail.com>
2022-07-11 18:56:15 -0700

    59 testParsingPrefixExpressions

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -7,6 +7,48 @@ import (
 	"github.com/sharpei/lexer"
 )
 
+// p 59
+func TestParsingPrefixExpressions(t *testing.T) {
+	prefixTests := []struct {
+		input string
+		operator string
+		integerValue int64
+	}{
+		{"!5;", "!", 5},
+		{"-15", "-", 15}
+	}
+
+	for _, tt := range prefixTests {
+		l := lexer.New(tt.input)
+		p := New(l)
+		program := p.ParseProgram()
+		checkParserErrors()
+
+		if len(program.Statements) != 1 {
+			t.Fatalf("program.Statements does not contain %d statements. got=%d\n",
+		1, len(program.Statements)
+		}
+
+		stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+		if !ok {
+			t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got=%T",
+		program.Statements[0])
+		}
+
+		exp, ok := stmt.Expression.(*ast.PrefixExpression)
+		if !ok {
+			t.Fatalf("stmt is not ast.PrefixExpression. got=%T", stmt.Expression)
+		}
+		if exp.Operator != tt.operator {
+			t.Fataf("exp.Operator is not '%s'. got=%s", tt.operator, exp.Operator)
+		}
+		if !testIntegerLiter(t, exp.Right, tt.intergerValue) {
+			return
+		}
+	}
+
+}
+
 // p 56
 func TestIntegerLiteralExpression(t *testing.T) {
 	input := "5;"
