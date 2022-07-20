461eb5c5f8a069c34f078551779ac9a92bf30017
hayitsdavid <davehay93@gmail.com>
2022-07-19 22:18:50 -0700

    if statements

--- ast/ast.go
+++ ast/ast.go
@@ -72,8 +72,8 @@ func (ie *IfExpression) String() string {
 }
 
 type BlockStatement struct {
-	Token     token.Token
-	Statement []Statement
+	Token      token.Token
+	Statements []Statement
 }
 
 func (bs *BlockStatement) statementNode()       {}
@@ -81,7 +81,7 @@ func (bs *BlockStatement) TokenLiteral() string { return bs.Token.Literal }
 func (bs *BlockStatement) String() string {
 	var out bytes.Buffer
 
-	for _, s := range bs.Statement {
+	for _, s := range bs.Statements {
 		out.WriteString(s.String())
 	}
 
