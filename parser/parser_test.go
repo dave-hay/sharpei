a56bc8de6ff01b5dcd4ff1ea30a6e603449b85c3
hayitsdavid <davehay93@gmail.com>
2022-07-20 16:51:20 -0700

    removed duplicates

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -8,49 +8,6 @@ import (
 	"github.com/sharpei/lexer"
 )
 
-// 78
-func testLiteralExpression(
-	t *testing.T,
-	exp ast.Expression,
-	expected interface{},
-) bool {
-	switch v := expected.(type) {
-	case int:
-		return testIntegerLiteral(t, exp, int64(v))
-	case int64:
-		return testIntegerLiteral(t, exp, v)
-	case string:
-		return testIdentifier(t, exp, v)
-	}
-	t.Errorf("type of exp not handled. got=%T", exp)
-	return false
-}
-
-func testInfixExpression(t *testing.T, exp ast.Expression, left interface{},
-	operator string, right interface{}) bool {
-
-	opExp, ok := exp.(*ast.InfixExpression)
-	if !ok {
-		t.Errorf("exp is not ast.OperatorExpression. got=%T(%s)", exp, exp)
-		return false
-	}
-
-	if !testLiteralExpression(t, opExp.Left, left) {
-		return false
-	}
-
-	if opExp.Operator != operator {
-		t.Errorf("exp.Operator is not '%s'. got=%q", operator, opExp.Operator)
-		return false
-	}
-
-	if !testLiteralExpression(t, opExp.Right, right) {
-		return false
-	}
-
-	return true
-}
-
 // 85
 func TestIfExpression(t *testing.T) {
 	input := `if (x < y) { x }`
@@ -121,6 +78,7 @@ func testIdentifier(t *testing.T, exp ast.Expression, value string) bool {
 	return true
 }
 
+// 78
 func testLiteralExpression(
 	t *testing.T,
 	exp ast.Expression,
