649bd030758def68c9894c0a56a9cb112d467b4e
hayitsdavid <davehay93@gmail.com>
2022-09-21 09:47:47 -0700

    eval boolean

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,13 @@ import (
 	"github.com/sharpei/object"
 )
 
+var (
+	// create reference rather than creating new objects
+	NULL  = &object.Null{}
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 
@@ -17,6 +24,8 @@ func Eval(node ast.Node) object.Object {
 		// Expressions
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return &object.Boolean{Value: node.Value}
 	}
 
 	return nil
@@ -30,4 +39,11 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	}
 
 	return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -8,6 +8,18 @@ import (
 	"github.com/sharpei/parser"
 )
 
+func testEval(input string) object.Object {
+	l := lexer.New(input)
+	p := parser.New(l)
+	program := p.ParseProgram()
+
+	return Eval(program)
+
+}
+
+/***************
+*** TEST INTEGER
+****************/
 func TestEvalIntegerExpression(t *testing.T) {
 	tests := []struct {
 		input    string
@@ -23,15 +35,6 @@ func TestEvalIntegerExpression(t *testing.T) {
 	}
 }
 
-func testEval(input string) object.Object {
-	l := lexer.New(input)
-	p := parser.New(l)
-	program := p.ParseProgram()
-
-	return Eval(program)
-
-}
-
 func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
 	result, ok := obj.(*object.Integer)
 	if !ok {
@@ -42,5 +45,38 @@ func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
 		t.Errorf("object has wrong value. got=%d, want=%d", result.Value, expected)
 		return false
 	}
+	return true
+}
+
+/***************
+*** TEST Boolean
+****************/
+
+func TestEvalBooleanExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"true", true},
+		{"false", false},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
+}
+
+func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
+	result, ok := obj.(*object.Boolean)
+
+	if !ok {
+		t.Errorf("object is not Boolean. got=%T (%+v)", obj, obj)
+
+		if result.Value != expected {
+			t.Errorf("object has wrong value. got=%t, want=%t", result.Value, expected)
+		}
+	}
+
 	return true
 }
