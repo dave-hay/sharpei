f74671713d96e9f50b8e4fc22537dfcde22c2a57
hayitsdavid <davehay93@gmail.com>
2022-09-21 22:24:06 -0700

    prefix funcs

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -24,6 +24,9 @@ func Eval(node ast.Node) object.Object {
 		// Expressions
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
 	case *ast.Boolean:
 		return &object.Boolean{Value: node.Value}
 	case *ast.InfixExpression:
@@ -35,13 +38,46 @@ func Eval(node ast.Node) object.Object {
 	return nil
 }
 
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	case "-":
+		return evalMinusPrefixOperatorExpression(right)
+	default:
+		return NULL
+	}
+}
+
+func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.INTEGER_OBJ {
+		return NULL
+	}
+
+	value := right.(*object.Integer).Value
+	return &object.Integer{Value: -value}
+}
+
 func evalInfixExpression(
 	operator string,
 	left, right object.Object,
 ) object.Object {
 	switch {
 	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
-		return evalInfixExpression(operator, left, right)
+		return evalIntegerInfixExpression(operator, left, right)
 	default:
 		return NULL
 	}
